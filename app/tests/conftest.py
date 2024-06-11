f414d999f56e18560d2ed379e089717b3a50c256
pynpassos <pinheiropassosm@gmail.com>
2024-06-11 17:30:40 -0300

    chore: Configurations to tests environment

---
+++ app/tests/conftest.py
@@ -0,0 +1,48 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+
+from app.database import Base
+from app.dependencies import get_db
+from app.main import app
+
+SQLALCHEMY_DATABASE_URL = "sqlite://"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+
+@pytest.fixture(scope="function")
+def db_session():
+    """Create a new database session with a rollback at the end of the test."""
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = TestingSessionLocal(bind=connection)
+    yield session
+    session.close()
+    transaction.rollback()
+    connection.close()
+
+
+@pytest.fixture(scope="function")
+def test_client(db_session):
+    """Create a test client that uses the override_get_db fixture to return a session."""
+
+    def override_get_db():
+        try:
+            yield db_session
+        finally:
+            db_session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    with TestClient(app) as test_client:
+        yield test_client
